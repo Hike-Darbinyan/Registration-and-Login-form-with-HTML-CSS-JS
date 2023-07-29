d31e63d03c0941eaf27b1508cba7177f69d4eb94
Hike Darbinyan <hike19872@gmail.com>
2023-07-29 13:38:16 +0400

    add info.txt

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('Public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname , '/login.html'));
+});
+
+app.post('/register', (req, res) => {
+  const { username, email, password } = req.body;
+  const data = `\nUsername: ${username}\nEmail: ${email}\nPassword: ${password}\n`;
+
+  fs.appendFile('info.txt', data, (err) => {
+    if(err) throw err;
+    console.log('User registered and data appended to info.txt')
+  });
+
+  res.send('Registration successsful!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
